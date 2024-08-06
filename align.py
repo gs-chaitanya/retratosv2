0520e0ffc770c374ff7b960d1f858fcee1a0851a
Chaitanya Gambali <gschaitanya2003@gmail.com>
2024-08-06 18:52:30 +0530

    dropped default arguments in align.py

--- align.py
+++ align.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
 
-def align_file(workdir, file = "alignment_file.txt", priors="latest.priors", output="alignments.fwd" ):
+def align_file(workdir, file, priors, output ):
     os.chdir(workdir)
     subprocess.run([f'eflomal-align -i {file} --priors {priors} --model 3 -f {output}'], shell=True)  # omitting reverse links -r en-sv.small.rev
