b4cf473e26052a16aa188186a6a81bf830ae0901
chirag <gschaitanya2003@gmail.com>
2024-08-19 01:10:41 +0530

    updated README and improved help section in the command line interface

--- retratos.py
+++ retratos.py
@@ -14,30 +14,30 @@ parser = argparse.ArgumentParser(prog="retratosv2",
                                 epilog="Thanks for using retratos :)")
 
     #parser arguments
-parser.add_argument('--mode', help='set working mode - choose out of align or filter', \
+parser.add_argument('--mode', help='set working mode - choose out of generate_priors, suggest, filter', \
                     required=True, choices = ["align", "filter", "generate_priors", "suggest"])
 
-parser.add_argument('--workdir',required=True, help='specify the working directory')
+parser.add_argument('--workdir',required=True, help='specify the working directory. all your working files are saved here and default files are automatically picked up from the workdir')
 
 parser.add_argument('--tag-config', help='specify a config file that \
-                    details which tags are to be included in the priors') #required=True - needed
+                    details which tags are to be included in the priors. It is a json file that needs to be kept in the workdir') #required=True - needed
 
     # align mode - align a small corpus using priors
-parser.add_argument('--file', help='corpus in fast align format')
+parser.add_argument('--file', help='specify a custom input file in any mode in case you do not want to go with the default file in the workdir')
 
     # filter mode - filters the priors file using given constraints for dictionary induction
-parser.add_argument('--min_freq', help='min frequency to include')
+parser.add_argument('--min_freq', help='min frequency below which the priors are excluded from the filtered priors')
 parser.add_argument('--priors', help='specify priors file - if not given default priors file in the workspace is used')
-parser.add_argument('--keep_top', help='specify priors file - if not given default priors file in the workspace is used')
-parser.add_argument('--include_unknowns', help='specify priors file - if not given default priors file in the workspace is used')
+parser.add_argument('--keep_top', help='after setting a minimum frequency, you can choose to keep only the top n priors sorted by frequency, where n is specified by this argument')
+parser.add_argument('--include_unknowns', help='Keep \'Yes\' to include unknown words in the filtered priors, default is \'No\'')
 
     # generate-priors mode - will align two text files in the fast align format - s1 ||| s2
-parser.add_argument('--left', help='source language corpus')
-parser.add_argument('--right', help='target language corpus')
-parser.add_argument("--lang_dir")
-parser.add_argument("--left_lang")
-parser.add_argument("--right_lang")
-parser.add_argument("--output")
+parser.add_argument('--left', help='specify source language corpus')
+parser.add_argument('--right', help='specify target language corpus')
+parser.add_argument("--lang_dir", help='path to the apertium language pair repository on your system')
+parser.add_argument("--left_lang", help='specify the source(left) language')
+parser.add_argument("--right_lang", help='specify the target(right) language')
+parser.add_argument("--output", help='specify an output filename instead of the default in any of the modes')
 
 args = vars(parser.parse_args())
 
@@ -95,10 +95,11 @@ if(args['mode'] == 'align'):
 # suggest mode is used to create bidix patches from filtered priors - uses arguents : file, workdir, output
 if(args['mode'] == 'suggest'):
     if (args['file'] == None):
-        print("Priors file not specified so proceeding with the workspace defaults")
+        print("Priors file not specified so proceeding with the workspace defaults \n")
         if (os.path.exists(f"{args['workdir']}/filtered.priors")):
             gen_bidix_patch(f"{args['workdir']}/filtered.priors", args['workdir'])
         else:
-            print("you have not provided a path to filtered priors and the default file does not exist. \nFirst generate the filtered priors or provide the correct pathname.")
+            print("Bidix patch generation failed")
+            print("You have not provided a path to filtered priors and the default file does not exist. \nFirst generate the filtered priors or provide the correct pathname.")
     else:
         gen_bidix_patch(args['file'], args['workdir'])
